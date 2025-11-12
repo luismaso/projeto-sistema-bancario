fddb1db7e68426e331388a7bd7c22c71c997c851
luishenriquemaso@gmail.com <luishenriquemaso@gmail.com>
2025-11-11 21:36:42 -0300

    Estrutura inicial do projeto com Spring Boot

---
+++ src/main/java/br/com/cesurgmarau/projeto_sistema_bancario/ProjetoSistemaBancarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesurgmarau.projeto_sistema_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSistemaBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSistemaBancarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cesurgmarau/projeto_sistema_bancario/ProjetoSistemaBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesurgmarau.projeto_sistema_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSistemaBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
